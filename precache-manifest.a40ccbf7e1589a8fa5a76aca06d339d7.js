0e2ae2b7e87355c48932bd65338bcf4ec150b6fe
Xcose <mfecanexhanti@gmail.com>
2020-04-23 21:35:31 +0200

    Updates

---
+++ precache-manifest.a40ccbf7e1589a8fa5a76aca06d339d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5aec39177cceaf7d83bf7c0f1bd549",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "44fa94dcc8b44273f583",
+    "url": "/tictactoe/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "8f0cc413a7ca56142e64",
+    "url": "/tictactoe/static/css/main.0b7ef6c0.chunk.css"
+  },
+  {
+    "revision": "44fa94dcc8b44273f583",
+    "url": "/tictactoe/static/js/2.5f108e44.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tictactoe/static/js/2.5f108e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0cc413a7ca56142e64",
+    "url": "/tictactoe/static/js/main.77e22958.chunk.js"
+  },
+  {
+    "revision": "8687869e2ce7edca0a72",
+    "url": "/tictactoe/static/js/runtime-main.2e4bb3d8.js"
+  }
+]);
